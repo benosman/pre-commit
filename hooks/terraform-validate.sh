639ea796e524837fc875a117e61c38e279dd2b39
Travis Johnson <travis@thisguy.codes>
2023-11-20 01:05:25 -0500

    🐛 (terraform-validate): Error on any failure
    current version only errors if the *last* validate command failed; but we should error if *any* validate command fails. This commit adds all errors and errors at the end if there were any at all.

--- hooks/terraform-validate.sh
+++ hooks/terraform-validate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 # OSX GUI apps do not pick up environment variables the same way as Terminal apps and there are no easy solutions,
 # especially as Apple changes the GUI app behavior every release (see https://stackoverflow.com/q/135688/483528). As a
@@ -10,15 +10,19 @@ export PATH=$PATH:/usr/local/bin
 # Disable output not usually helpful when running in automation (such as guidance to run plan after init)
 export TF_IN_AUTOMATION=1
 
-# Store and return last failure from validate so this can validate every directory passed before exiting
+# Store and return all failures from validate so this can validate every directory passed before exiting
 VALIDATE_ERROR=0
 
 for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
   echo "--> Running 'terraform validate' in directory '$dir'"
   pushd "$dir" >/dev/null
-  terraform init -backend=false || VALIDATE_ERROR=$?
-  terraform validate || VALIDATE_ERROR=$?
+  terraform init -backend=false || VALIDATE_ERROR=$((VALIDATE_ERROR + $?))
+  terraform validate || VALIDATE_ERROR=$((VALIDATE_ERROR + $?))
   popd >/dev/null
 done
 
-exit ${VALIDATE_ERROR}
+if [[ $VALIDATE_ERROR -gt 0 ]]; then
+  exit 1
+else
+  exit 0
+fi
